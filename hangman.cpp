ec067094b99af3786a88c2ea2fb0d10e9850d459
Akshat Pandey <akshatp1903@gmail.com>
2024-07-26 20:02:35 +0530

    hangman states and helper functions for guessing

--- hangman.cpp
+++ hangman.cpp
@@ -23,3 +23,47 @@ std::string chooseWord(const std::vector<std::string>& wordList) {
     int index = rand() % wordList.size();
     return wordList[index];
 }
+
+void displayGameState(const std::string& word, const std::string& guessedLetters) {
+    for(char c : word){
+        if(guessedLetters.find(c) != std::string::npos){
+            std::cout << c << ' ';
+        }else{
+            std::cout << "_ ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+bool isWordGuessed(const std::string& word, const std::string& guessedLetters){
+    for(char c : word){
+        if(guessedLetters.find(c) == std::string::npos){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isGuessCorrect(const std::string& word, char guess){
+    return word.find(guess) != std::string::npos;
+}
+
+void updateGuessedLetters(std::string& guessedLetters, char guess){
+    guessedLetters += guess;
+}
+
+void displayHangman(int wrongGuesses) {
+    static const std::vector<std::string> hangmanstates = {
+        "  +---+\n      |\n      |\n      |\n      |\n      |\n=========",
+        "  +---+\n  O   |\n      |\n      |\n      |\n      |\n=========",
+        "  +---+\n  O   |\n  |   |\n      |\n      |\n      |\n=========",
+        "  +---+\n  O   |\n /|   |\n      |\n      |\n      |\n=========",
+        "  +---+\n  O   |\n /|\\  |\n      |\n      |\n      |\n=========",
+        "  +---+\n  O   |\n /|\\  |\n /    |\n      |\n      |\n=========",
+        "  +---+\n  O   |\n /|\\  |\n / \\  |\n      |\n      |\n========="
+    };
+}
+
+int main() {
+    
+}
