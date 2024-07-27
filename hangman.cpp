7211cce8479180b3f2912a3a2049c93012b6f358
Akshat Pandey <akshatp1903@gmail.com>
2024-07-28 04:25:09 +0530

    Minor changes

--- hangman.cpp
+++ hangman.cpp
@@ -4,28 +4,26 @@
 #include <ctime>
 #include <cstdlib>
 #include <algorithm>
-#include <fstream> 
+#include <fstream>
 #include <cctype>
 
 std::vector<std::string> readWordsFromFile(const std::string& filename) {
     std::vector<std::string> wordList;
-    std::ifstream file(filename);
+    std::ifstream file;
+    file.open(filename);
     std::string line;
 
     while (std::getline(file, line)) {
-        // Remove any trailing whitespace
+        
         line.erase(std::find_if(line.rbegin(), line.rend(), [](unsigned char ch) {
             return !std::isspace(ch);
         }).base(), line.end());
 
-        // Convert to lowercase
-        std::transform(line.begin(), line.end(), line.begin(), ::tolower);
-        
         if (!line.empty()) {
             wordList.push_back(line);
         }
     }
-
+    file.close();
     return wordList;
 }
 
@@ -37,37 +35,41 @@ std::string chooseWord(const std::vector<std::string>& wordList) {
 void displayGameState(const std::string& word, const std::string& guessedLetters) {
     for (char c : word) {
         if (std::isalpha(c)) {
-            if (guessedLetters.find(c) != std::string::npos) {
+            if (guessedLetters.find(std::tolower(c)) != std::string::npos) {
                 std::cout << c << ' ';
             } else {
                 std::cout << "_ ";
             }
         } else {
-            std::cout << c << ' ';  // Display non-alphabetic characters as is
+            std::cout << c << ' ';  
         }
     }
     std::cout << std::endl;
 }
 
-bool isWordGuessed(const std::string& word, const std::string& guessedLetters){
-    for(char c : word){
-        if(std::isalpha(c) && guessedLetters.find(c) == std::string::npos){
+bool isWordGuessed(const std::string& word, const std::string& guessedLetters) {
+    for (char c : word) {
+        if (std::isalpha(c) && guessedLetters.find(std::tolower(c)) == std::string::npos) {
             return false;
         }
     }
     return true;
 }
 
-bool isGuessCorrect(const std::string& word, char guess){
-    return word.find(guess) != std::string::npos;
+bool isGuessCorrect(const std::string& word, char guess) {
+    return std::any_of(word.begin(), word.end(), [guess](char c) {
+        return std::tolower(c) == std::tolower(guess);
+    });
 }
 
-void updateGuessedLetters(std::string& guessedLetters, char guess){
-    guessedLetters += guess;
+void updateGuessedLetters(std::string& guessedLetters, char guess) {
+    if (guessedLetters.find(guess) == std::string::npos) {
+        guessedLetters += guess;
+    }
 }
 
 void displayHangman(int wrongGuesses) {
-    static const std::vector<std::string> hangmanstates = {
+    static const std::vector<std::string> hangmanStates = {
         "  +---+\n      |\n      |\n      |\n      |\n      |\n=========",
         "  +---+\n  O   |\n      |\n      |\n      |\n      |\n=========",
         "  +---+\n  O   |\n  |   |\n      |\n      |\n      |\n=========",
@@ -76,53 +78,54 @@ void displayHangman(int wrongGuesses) {
         "  +---+\n  O   |\n /|\\  |\n /    |\n      |\n      |\n=========",
         "  +---+\n  O   |\n /|\\  |\n / \\  |\n      |\n      |\n========="
     };
-    std::cout << hangmanstates[wrongGuesses] << std::endl;
+    std::cout << hangmanStates[wrongGuesses] << std::endl;
 }
 
 int main() {
-    std::vector<std::string> wordList = readWordsFromFile ("movies.txt");
-    if (wordList.empty()){
-        std::cerr << "Word List is empty or File not Found!!" << std::endl;
+    std::vector<std::string> wordList = readWordsFromFile("movies.txt");
+    if (wordList.empty()) {
+        std::cerr << "Word list is empty or file not found!" << std::endl;
         return 1;
     }
 
     srand(static_cast<unsigned int>(time(0)));
-    
+
     char playAgain;
-    do{
+    do {
         std::string word = chooseWord(wordList);
         std::string guessedLetters;
         int wrongGuesses = 0;
         const int maxWrongGuesses = 6;
 
-        while(wrongGuesses < maxWrongGuesses && !isWordGuessed(word, guessedLetters)){
+        while (wrongGuesses < maxWrongGuesses && !isWordGuessed(word, guessedLetters)) {
             displayGameState(word, guessedLetters);
             displayHangman(wrongGuesses);
 
             std::cout << "Enter your guess: ";
             char guess;
             std::cin >> guess;
-            guess = tolower(guess);
+            guess = std::tolower(guess);
 
-            if(isGuessCorrect(word, guess)){
-                std::cout << "Guess is correct :)" << std::endl;
+            if (isGuessCorrect(word, guess)) {
+                std::cout << "Correct guess!" << std::endl;
                 updateGuessedLetters(guessedLetters, guess);
-            }else{
-                std::cout << "Incorrect Guess :(" << std::endl;
+            } else {
+                std::cout << "Wrong guess!" << std::endl;
                 ++wrongGuesses;
             }
         }
 
-        if(isWordGuessed(word, guessedLetters)){
-            std::cout << "Congrats!! You've guessed the word: " << word << std::endl;
-        }else{
-            std::cout << "Unluckyy, You've been hanged! The word was: " << word << std::endl;
+        if (isWordGuessed(word, guessedLetters)) {
+            std::cout << "Congratulations! You've guessed the movie: " << word << std::endl;
+        } else {
+            std::cout << "You've been hanged! The movie was: " << word << std::endl;
         }
 
-        std::cout << "Do you wanna play again? (y/n): ";
+        std::cout << "Play again? (y/n): ";
         std::cin >> playAgain;
-        playAgain = tolower(playAgain);
-    }while(playAgain == 'y');
+        playAgain = std::tolower(playAgain);
+    } while (playAgain == 'y');
+
 
     return 0;
 }
